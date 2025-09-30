058d1ba1462da2c1eb4fbc42a01caca35136109b
Austine-john <ochiengaustine582@gmail.com>
2025-09-30 10:42:57 +0300

    property decorators

--- lib/dog.py
+++ lib/dog.py
@@ -9,31 +9,29 @@ APPROVED_BREEDS = [
     "Pointer"
 ]
 
-
 class Dog:
-    def __init__(self, name='Fido', breed='Mastiff'):
-        self.name = name
-        self.breed = breed
+    def __init__(self, name, breed):
+        self.name = name      # ✅ goes through setter validation
+        self.breed = breed    # ✅ goes through setter validation
 
-    def get_name(self):
+    @property
+    def name(self):
         return self._name
 
-    def set_name(self, name):
-        if isinstance(name, str) and 1 <= len(name) <= 25:
-            self._name = name.title()
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
         else:
-            raise ValueError(
-                "Name must be string between 1 and 25 characters.")
+            raise ValueError("Name must be a string between 1 and 25 characters.")
 
-    name = property(get_name, set_name)
-
-    def get_breed(self):
+    @property
+    def breed(self):
         return self._breed
 
-    def set_breed(self, breed):
-        if breed in APPROVED_BREEDS:
-            self._breed = breed
+    @breed.setter
+    def breed(self, value):
+        if value in APPROVED_BREEDS:
+            self._breed = value
         else:
-            raise ValueError("Breed must be in list of approved breeds.")
-
-    breed = property(get_breed, set_breed)
+            raise ValueError("Breed must be an approved choice.")
